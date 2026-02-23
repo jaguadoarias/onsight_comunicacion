d514542d7041c46563062aaf049b1d2a5f3dabde
Javier Aguado Arias <jaguadoarias@gmail.com>
2026-02-23 12:43:39 +0100

    fix: fixing url with hash

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import Services from "../components/Services/Services";
@@ -8,6 +10,16 @@ import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
 
 export default function Home() {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    if (!hash) return;
+    const el = document.querySelector(hash);
+    if (el) {
+      el.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [hash]);
+
   return (
     <>
       <Navbar />
